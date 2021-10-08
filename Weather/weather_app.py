e503d6ca143f9428dc112dda6d70108d36b7c97e
Sherwyn Fernandes <62534363+sherwyn4real@users.noreply.github.com>
2021-10-08 20:44:31 +0530

    Update weather_app.py

--- Weather/weather_app.py
+++ Weather/weather_app.py
@@ -35,6 +35,29 @@ def search_cord_pin(l):
     
     if(type(l) == list):
         url=f'http://api.openweathermap.org/data/2.5/weather?lat={l[0]}&lon={l[1]}&appid={KEY}&units=metric'
+        response = requests.get(url)
+        if response.status_code in (200,202):
+            res = response.json()
+            output={
+            "country":res["sys"]["country"],
+            "name":res["name"],
+            "temp":res["main"]["temp"],
+            "min_temp": res["main"]["temp_min"],
+            "max_temp": res["main"]["temp_max"],
+            "latitude":res["coord"]["lat"],
+            "longitude": res["coord"]["lon"]
+            }
+            
+            return output,200
+            
+        elif response.status_code == 400:
+            return {'status':response.status_code, 'message':'Bad request'}
+        else:
+           return report_error,response.status_code
+        
+        
+        '''if(type(l) == list):
+        url=f'http://api.openweathermap.org/data/2.5/weather?lat={l[0]}&lon={l[1]}&appid={KEY}&units=metric'
         response = requests.get(url).json()
         if response.get('cod') != 200:
             return report_error,404
@@ -49,7 +72,7 @@ def search_cord_pin(l):
             "latitude":response["coord"]["lat"],
             "longitude": response["coord"]["lon"]
             }
-            return output,200
+            return output,200'''
             
     else:
         url=f'http://api.openweathermap.org/data/2.5/weather?zip={l},IN&appid={KEY}&units=metric'
